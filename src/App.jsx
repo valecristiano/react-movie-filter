6d19d7e66aa31b87b80019d3775f61202bacbe48
valecristiano <valeriamdcristiano@gmail.com>
2026-02-12 19:10:48 +0300

    new film form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const filmList = [
+const filmGenreList = [
   { title: "Inception", genre: "Fantascienza" },
   { title: "Il Padrino", genre: "Thriller" },
   { title: "Titanic", genre: "Romantico" },
@@ -12,27 +12,38 @@ const filmList = [
 // array di generi senza ripetizioni
 const genreList = [];
 
-for (const film of filmList) {
+for (const film of filmGenreList) {
   if (!genreList.includes(film.genre)) {
     genreList.push(film.genre);
   }
 }
 
 export default function App() {
+  const [filmList, setFilmList] = useState(filmGenreList);
   const [genreSelected, setGenreSelected] = useState("");
   const [filmSearch, setFilmSearch] = useState("");
+  const [filmAdded, setFilmAdded] = useState("");
 
   let filteredFilmList = filmList;
 
   //filtri per tipo di genere con select e nome del film con input
 
   if (genreSelected !== "") {
-    filteredFilmList = filmList.filter((film) => film.genre === genreSelected);
+    filteredFilmList = filteredFilmList.filter((film) => film.genre === genreSelected);
   }
 
   if (filmSearch !== "") {
-    filteredFilmList = filmList.filter((film) => film.title.toLocaleLowerCase().includes(filmSearch.toLowerCase()));
+    filteredFilmList = filteredFilmList.filter((film) => film.title.toLocaleLowerCase().includes(filmSearch.toLowerCase()));
   }
+
+  const addNewFilm = (e) => {
+    e.preventDefault();
+
+    if (!filmAdded) return;
+
+    setFilmList([...filmList, { title: filmAdded, genre: "" }]);
+    setFilmAdded("");
+  };
 
   return (
     <>
@@ -70,6 +81,20 @@ export default function App() {
             Cerca per titolo:
           </label>
           <input value={filmSearch} onChange={(e) => setFilmSearch(e.target.value)} className="form-control" type="text" name="title-search" id="title-search" />
+        </form>
+      </section>
+      {/* seziona aggiunta titolo */}
+      <section className="container my-3">
+        <form action="#" onSubmit={addNewFilm}>
+          <label className="form-label" htmlFor="title-addition">
+            Aggiungi titolo:
+          </label>
+          <div className="input-group mb-3">
+            <input value={filmAdded} onChange={(e) => setFilmAdded(e.target.value)} className="form-control" type="text" name="title-addition" id="title-addition" />
+            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">
+              Aggiungi
+            </button>
+          </div>
         </form>
       </section>
     </>
