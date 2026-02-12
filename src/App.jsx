45d83715d3830acacb6e9434a26da9d7fe8929d4
valecristiano <valeriamdcristiano@gmail.com>
2026-02-12 17:47:41 +0300

    genre list with no repetitions

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,24 @@ const filmList = [
   { title: "Pulp Fiction", genre: "Thriller" },
 ];
 
+// array di generi senza ripetizioni
+const genreList = [];
+
+for (const film of filmList) {
+  console.log(film.genre);
+  if (!genreList.includes(film.genre)) {
+    genreList.push(film.genre);
+  }
+}
+
+console.log("genreList", genreList);
+
 export default function App() {
   const [genreSelected, setGenreSelected] = useState("");
 
+  //lista film filtrata per genere selezionato nella select
   const filteredFilmList = filmList.filter((film) => film.genre === genreSelected);
-  console.log(filteredFilmList);
+  console.log("filteredFilmList", filteredFilmList);
 
   return (
     <>
@@ -22,6 +35,7 @@ export default function App() {
           <h1>Movie filter</h1>
         </div>
       </header>
+      {/* lista film mappati sulla lista filtrata per genere */}
       <section className="container">
         <ul>
           {filteredFilmList.map((film, index) => (
@@ -31,12 +45,14 @@ export default function App() {
           ))}
         </ul>
       </section>
+      {/* select mappata sull'array di generi senza ripetizioni */}
       <section className="container">
         <label htmlFor="genre-selection">Filtra per genere</label>
         <select value={genreSelected} onChange={(e) => setGenreSelected(e.target.value)} id="genre-selection" className="form-select" aria-label="Default select example">
-          {filmList.map((film, index) => (
-            <option key={index} value={film.genre}>
-              {film.genre}
+          <option value="seleziona">Seleziona un genere</option>
+          {genreList.map((genre, index) => (
+            <option key={index} value={genre}>
+              {genre}
             </option>
           ))}
         </select>
